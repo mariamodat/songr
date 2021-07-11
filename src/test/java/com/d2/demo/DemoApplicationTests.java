374e3bc22e339218f96f8eb73926e855877ad2eb
mariamodat <mariamodat0@gmail.com>
2021-07-11 21:36:29 +0300

    edited tests

--- src/test/java/com/d2/demo/DemoApplicationTests.java
+++ src/test/java/com/d2/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.d2.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void constructorTest() {
+
+    Album album = new Album("Hello from test! ","Mariam","11","2min","xxxx");
+    Assertions.assertEquals("Hello from test! ",album.title);
+    Assertions.assertEquals("Mariam",album.artist);
+    Assertions.assertEquals("11",album.songCount);
+    Assertions.assertEquals("2min",album.length);
+    Assertions.assertEquals("xxxx",album.imageUrl);
+
+  }
 
 }
